5ad81a2e7fefc970a8bb9081d4833f4d8c4f36e3
jschneiderm98 <julio_schneider@hotmail.com>
2021-05-15 23:40:40 -0300

    started side-bar

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Icon } from '@iconify/react';
+import homeOutlined from '@iconify-icons/ant-design/home-outlined';
+import productDownloadble from '@iconify-icons/gridicons/product-downloadable';
+
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
 import './styles.css';
 
 const Home = () => (
-  <div className="home">Alguma coisa</div>
+  <div className="home">
+    <div className="home-container">
+      <nav className="side-bar">
+        <Logo />
+        <ul>
+          <li className="side-bar-item selected">
+            <Link to="/">
+              <Icon icon={homeOutlined} />
+              <p>Dashboard</p>
+            </Link>
+          </li>
+          <li className="side-bar-item">
+            <Link to="/">
+              <Icon icon={productDownloadble} />
+              <p>Pacientes</p>
+            </Link>
+          </li>
+          <li className="side-bar-item">
+            <Link to="/">
+              <Icon icon={productDownloadble} />
+              <p>Testes</p>
+            </Link>
+          </li>
+          <li className="side-bar-item">
+            <Link to="/">
+              <Icon icon={productDownloadble} />
+              <p>Configurações</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="home-body">a</div>
+    </div>
+  </div>
 );
 
 export default Home;
